a70e702739b6974b160d66e20ed4027fbcff6f7c
Dylan Hyatt <dylan.hyatt@gmail.com>
2015-06-28 10:37:25 -0700

    Starting the square class and 2D square array

--- 2D-Strategy-Game/2D-Strategy-Game/Square.cs
+++ 2D-Strategy-Game/2D-Strategy-Game/Square.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-
-
 namespace _2D_Strategy_Game
 {
-    class Grid
+    class Square
     {
-        private Square[][] squares;
     }
 }
